db0952e8918a6c0cf42261b8aff9b3d4fd889c0f
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-08 14:19:56 +0100

    Update MySensorEvent.kt

--- app/src/main/java/com/example/har/MySensorEvent.kt
+++ app/src/main/java/com/example/har/MySensorEvent.kt
@@ -2,10 +2,13 @@ package com.example.har
 
 enum class SensorType(i: Int) {
     Accelerometer(1),
+    STEP_COUNTER(2)
 }
 
 class MySensorEvent {
     var type = SensorType.Accelerometer
     var value = ""
     var data : FloatArray = FloatArray(3)
+    var dataStep_counter : FloatArray = FloatArray(1)
+
 }
